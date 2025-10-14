c164a1918f50b66be4c34d963aa6b241ad7f4c29
Bala <balasudahn17@gmail.com>
2025-10-14 13:54:58 +0530

    updated

---
+++ JS/intro_variable_datatype/intro.js
@@ -0,0 +1,99 @@
+// var a=10;
+// console.log(a);
+// // Variable key word && same Variable name
+// var a=12;
+// console.log(a);
+
+// let a=10;
+// let a=12;
+// console.log(a);
+
+
+// Reassign 
+
+// let a = "this is data"
+
+// a="This is not data";
+
+// console.log(a);
+
+
+// const aa = 10 
+
+// console.log(aa);
+
+
+// console.log(aa+1);
+
+// let a = 10
+
+// console.log(a++);
+
+// a=a+1
+
+// console.log(a++);
+
+// console.log(a++);
+
+// let newdatas = a+=9
+
+// a=10+9
+
+// a=a+1
+
+// a=a*1
+
+
+
+
+// var a = 10
+// var b = 10
+// var c = a+b
+
+// c ==20
+
+
+
+
+// =    
+// +=
+// *=
+// -=
+
+5+10
+
+console.log(5+10);
+
+
+
+const result = 20*13
+
+console.log(result);
+
+
+// const heading = "this is the express"
+
+
+// var a = 10;
+// console.log();
+
+
+// []
+// {}
+
+// " "0n
+
+// "" or ''
+
+
+
+// var a = NaN
+
+
+
+// console.log("hello" * 5);
+
+
+// var a = "this is data"
+
+// console.log("java" - "Java Script");
