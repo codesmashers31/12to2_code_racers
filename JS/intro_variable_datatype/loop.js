6301e677c55729bbb3a3b45f2866dd96942dfc7f
Bala <balasudahn17@gmail.com>
2025-10-15 13:55:16 +0530

    new loop

---
+++ JS/intro_variable_datatype/loop.js
@@ -0,0 +1,77 @@
+// console.log('This is loop');
+
+// let a = 30
+
+
+// for(initialization;condtion;decriment/increment){
+
+//     //code exculation part 
+//     //statement
+// }
+
+// while(condtion){
+//     statements
+//     decrement/increment
+
+// }
+
+
+
+// do{
+
+//     // decrement/increment
+
+// }while(condition)
+
+
+
+
+
+// for(let a=32;a<=30;a++){
+
+// console.log(30+1);
+
+    
+
+// }
+
+// let a = true
+
+// while(a){
+
+//     console.log(a);
+
+//     a=false
+    
+   
+// }
+
+// let count = 10 
+
+// do{
+//    console.log("Running even if its false");
+//    count++
+   
+// }while(count < 5)
+
+
+
+
+
+
+for(let i = 1;i<=a;i++){
+
+    //console.log('Normal Number',i);
+
+    // if(i%2===0){
+    //     console.log("Even number",i);
+        
+    // }else{
+    //     console.log('Odd Number',i);
+    // }
+
+    console.log(i*2);
+    
+    
+        
+}
