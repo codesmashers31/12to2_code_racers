6b813a600416ebf7d7138b8bc966d0d63012f211
Bala <balasudahn17@gmail.com>
2025-10-21 13:57:03 +0530

    udpated

--- JS/intro_variable_datatype/nonprimitive/main.js
+++ JS/intro_variable_datatype/nonprimitive/main.js
@@ -1,36 +1,120 @@
-// Array
+// // Array
 
-// var arr = [1,2,3];
+// // var arr = [1,2,3];
 
-// console.log(arr);
-// Object
+// // console.log(arr);
+// // Object
 
-let obj = {name:"HTML",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000}
-
-
-//console.log(obj.Subject.Classroom);
-
-let arr = [{name:"Css",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000},{name:"HTML",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000}]
+// let obj = {name:"HTML",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000}
 
 
-// console.log(arr[1].name);
+// //console.log(obj.Subject.Classroom);
+
+// let arr = [{name:"Css",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000},{name:"HTML",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000}]
 
 
-// const dats = arr.map((e)=>e.name)
-
-// console.log(dats);
+// // console.log(arr[1].name);
 
 
+// // const dats = arr.map((e)=>e.name)
 
-const add_1 = () => console.log('This is function');
+// // console.log(dats);
+
+
+
+// // const add_1 = () => console.log('This is function');
     
 
 
-// const add = (gjjfgjk,one)=>{
+// // // const add = (gjjfgjk,one)=>{
 
-//     console.log(gjjfgjk,one);
+// // //     console.log(gjjfgjk,one);
     
+
+// // // }
+
+// // // add("This 9")
+
+
+
+
+
+// function teamaker(tea){
+
+//     if(tea==='lemontea'){
+//              console.log('He Making the lemon Tea',tea); 
+//     }
+
+//     else if(tea==="blacktea"){
+//         console.log('He Making the lemon Tea',tea);
+        
+//     }else{
+
+//         console.log('Coffee is not avaliable');
+        
+//     }
 
 // }
 
-// add("This 9")
+
+// const payment  = function(payment){
+
+//   console.log('Payment done',payment);
+  
+
+
+// }
+
+
+
+// const waiter = ()=>{
+//     const  serve = 10
+
+//    console.log('Succfully deliver',serve);
+
+// }
+
+
+ 
+
+
+// waiter()
+
+// teamaker('lemontea')
+
+
+// payment(10)
+
+
+// var thisisnew = 10
+const array = ["this is new"]
+
+console.log(array[0][4]);
+
+
+const nrmalobj = {name:{name:"React",subject:"Node",price:2000}}
+
+const obj = [{name:"React",subject:"Node",price:2000},{name:"React",subject:"Node",price:2000}]
+
+
+
+var apps={messanger:["Whatsapp","fb","arattai"],Food:["swiggy,zomato"]
+    ,Travel:["makemytrip","ixigo"]}
+console.log(apps.Food[0][1])
+
+
+
+const a = false
+if(!a){
+
+    console.log('Condition done');
+    
+
+}else{
+    console.log('Condition Not Done');
+}
+
+
+
+
+const b =10;
