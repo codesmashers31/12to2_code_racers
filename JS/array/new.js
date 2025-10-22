f7715ff0ac8d2af37e50d79953b598d39dfeff75
codesmashers31 <balasoftlogic12@gmail.com>
2025-10-22 13:40:17 +0530

    Array methods
    Arraymentods

---
+++ JS/array/new.js
@@ -0,0 +1,145 @@
+// const arr = [1,2,3,4,5,6,6,8,9];
+
+
+// console.log('Original array',arr);
+
+//  arr.push(4,2)
+
+
+// console.log('After Push',arr);
+
+// Pop
+
+// const arr = [1,2,3,4,5,6,6,8,9];
+// console.log('Original array',arr);
+// arr.pop()
+
+// console.log('After Pop',arr);
+
+// shift
+// const arr = [1,2,3,4,5,6,6,8,9];
+
+// const a = arr.shift()
+// const b = arr.shift()
+// console.log(a,b);
+//unshit
+
+// const arr = [1,2,3,4,5,6,6,8,9];
+
+// arr.unshift(1,2,3,4,5)
+
+// console.log(arr);
+
+
+// concat
+
+// arr.concat(arr1)
+
+
+// const arr = [1,2,3,4,5,6,6,8,9];
+
+// const arr1 = [1,3,56,67];
+
+// const arr2 = [1,4,67]
+
+// let c = arr.concat(arr1,arr2)
+
+// console.log(c);
+
+
+
+// join()
+
+
+// const arr = [1,2,3,4,5,6,6,8,9];
+// const newarr = arr.join("This")
+
+// console.log(newarr);
+
+
+// // reverse()
+// let a = [1,2,3,4,5]
+// a.reverse()
+
+
+// Array.slice(start,end)
+
+
+// const a = ["text","new","thor","iron man"]
+
+
+// console.log(a);
+
+
+// const part = a.slice(0,3)
+
+
+// console.log(a);
+
+// console.log(part);
+
+
+
+// splice
+
+
+// Array.splice(start,deletecount,newarray)
+
+
+// const arr = ["MCU","DC","LCU","KVACU","VCU","TKCU"];
+
+// arr.splice(2,1)
+
+
+// console.log(arr);
+
+
+// includes()
+
+
+// const arr = [1,2,3,4,5]
+
+// const value =  arr.includes(0);
+
+// console.log(value);
+
+
+// indexof
+
+
+// const arr = [1,2,3,40,50]
+
+// const index = arr.indexOf(40)
+
+// console.log(index);
+
+
+// Higher Order Array Methods
+
+
+// syntax
+// const arr = [1,2,3,40,50]
+
+// arr.forEach((currentvalue,index)=>{
+
+//     console.log(currentvalue*2);
+    
+
+
+
+// });
+
+// map
+
+
+//    const arr = [1,2,3,40,623]
+
+//    arr.map((e,i)=>{
+
+//    console.log(e,i);
+   
+
+
+//    })
+
+
